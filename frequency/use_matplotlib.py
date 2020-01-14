7a1c9fc4ae296d173015122731475a5569b23717
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2020-01-14 14:30:33 -0600

    use matplotlib to plot data

---
+++ frequency/use_matplotlib.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Prepocess the files
+def file_handle():
+    f=open("text.txt","r")
+    contents =f.read()
+    contents = contents.lower()
+    contents = contents.replace(",", " ")
+    contents = contents.replace(".", " ")
+    contents = contents.replace("?", " ")
+    contents = contents.replace("!", " ")
+    contents = contents.split()
+    return contents
+    print(contents)
+
+# Find the TOP n frequency of the words
+def frequence(contents, n):
+    frequence = []
+    times = []
+    for i in contents:
+        if i not in frequence:
+            frequence.append(i)
+            times.append(contents.count(i))
+            times.sort()
+            times = times[::-1]
+    
+    # Find the Max frequency (n)       
+    top_frequence = times[:n]
+    top_frequence_words = []
+    for j in top_frequence:
+        for k in contents:
+            if contents.count(k) == j and k not in top_frequence_words:
+                #print("'{}': {}".format(k, j))
+                top_frequence_words.append(k)
+                break
+    
+    return top_frequence_words, top_frequence
+
+# Plot data
+def plot(l1, l2):
+    plt.bar(l1, l2)
+    plt.xlabel('Words')
+    plt.ylabel('Frequency')
+    plt.show()
+
+# Main function
+if __name__ == "__main__":
+    contents = file_handle()
+    top_frequence_words, top_frequence = frequence(contents, 20)
+    plot(top_frequence_words, top_frequence)
